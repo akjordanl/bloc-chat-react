0a2bcc4b598b7dc7aa6137b8fdc72517eba58894
Andrew Jordan <akjordanl@gmail.com>
2019-01-16 15:41:51 -0500

    checkpoint list chat rooms complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import { Route, Link } from 'react-router-dom';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDLavZrIRxo3q_YiSbxVl6HYe45iSVDbag",
+  authDomain: "bloc-chat-react-7e217.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-7e217.firebaseio.com",
+  projectId: "bloc-chat-react-7e217",
+  storageBucket: "bloc-chat-react-7e217.appspot.com",
+  messagingSenderId: "730942096337"
+};
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div class="grid-container">
+          <div id="mySidenav" class="sidenav">
+            <h1>Bloc Chat</h1>
+            <RoomList
+              firebase={firebase}
+            />
+          </div>
+          <div>
+            Rooms content will go here
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rooms: []
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      console.log(snapshot);
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
+    });
+  }
+
+    render () {
+      return (
+        <ul>
+          { this.state.rooms.map( (room) =>
+            <li> {room} </li>
+          )}
+        </ul>
+      )
+    };
+  }
+
+  export default RoomList;
